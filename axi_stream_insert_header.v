73725dabf047ea434460b27c2010624461e59316
eva-ljj <130600937+eva-ljj@users.noreply.github.com>
2023-04-14 20:12:25 +0800

    Update axi_stream_insert_header.v

--- axi_stream_insert_header.v
+++ axi_stream_insert_header.v
@@ -23,7 +23,7 @@ module axi_stream_insert_header #(
     output                      ready_insert
 );
     // Your code here
-    reg [$clog2(DATA_BYTE_WD):0] head_valid_num;
+    reg [DATA_BYTE_WD-1:0] head_valid_num;
     reg [     DATA_WD-1 : 0] header_tmp;
     reg [DATA_BYTE_WD-1 : 0] keep_in_tmp;
     reg [   2*DATA_WD-1 : 0] data_in_tmp;
@@ -35,7 +35,6 @@ module axi_stream_insert_header #(
     reg [DATA_BYTE_WD-1 : 0] keep_out_tmp;
     reg last_out_tmp;
     reg valid_out_tmp;
-    reg ready_out_tmp;
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
@@ -50,7 +49,6 @@ module axi_stream_insert_header #(
             valid_out_tmp <= 0;
             keep_out_tmp <= 0;
             keep_in_tmp <= 0;
-            ready_out_tmp <= 0;
         end else begin
             if(valid_insert && ready_insert)begin//tb里面，这两个信号同时拉高一个周期
                 output_over_flag <= 0;
@@ -60,43 +58,42 @@ module axi_stream_insert_header #(
                 last_out_tmp <= 0;
                 data_out_tmp <= 0;
                 ready_insert_tmp <= 0;
+                valid_out_tmp <= 0;
             end
-            else if(valid_in && ready_in && second_cycle && (~valid_insert)) begin
+            else if(valid_in && ready_in && second_cycle ) begin
                 data_in_tmp <= {header_tmp,data_in};
                 second_cycle <= 0;
-                valid_out_tmp <= 1;
                 data_out_tmp <= 0;
             end
             else if(valid_in && (~second_cycle) && (~input_over_flag) &&(~output_over_flag) && (~last_in)) begin
                 keep_out_tmp <= {DATA_BYTE_WD{1'b1}};
                 data_in_tmp <= {data_in_tmp[DATA_WD-1 : 0],data_in};
-                data_out_tmp <= data_in_tmp >> (8*head_valid_num);
+                data_out_tmp <= data_in_tmp >> multiply(head_valid_num,8);
                 input_over_flag <= 0;
                 valid_out_tmp <= 1;
             end
             else if(valid_in && (~second_cycle) && (~input_over_flag) &&(~output_over_flag) && last_in) begin
                 keep_out_tmp <= {DATA_BYTE_WD{1'b1}};
                 data_in_tmp <= {data_in_tmp[DATA_WD-1 : 0],data_in};
-                data_out_tmp <= data_in_tmp >> (8*head_valid_num);
+                data_out_tmp <= data_in_tmp >> multiply(head_valid_num,8);
                 input_over_flag <= 1;
                 valid_out_tmp <= 1;
                 keep_in_tmp <= keep_in;
             end
             else if(input_over_flag) begin
                 keep_out_tmp <= {DATA_BYTE_WD{1'b1}};
-                data_out_tmp <= data_in_tmp >> (8*head_valid_num);
+                data_out_tmp <= data_in_tmp >> multiply(head_valid_num,8);
                 output_over_flag <= 1;
-                ready_out_tmp <= 1;
                 ready_insert_tmp <= 1;
                 valid_out_tmp <= 1;
                 input_over_flag <= 0;
             end
             else if(output_over_flag) begin
-                data_out_tmp <= data_in_tmp << (8*(DATA_BYTE_WD-head_valid_num));
+                data_out_tmp <= data_in_tmp << multiply((DATA_BYTE_WD-head_valid_num),8);
                 keep_out_tmp <= keep_in_tmp << (DATA_BYTE_WD-head_valid_num);
                 
                 last_out_tmp <= 1;
-                valid_out_tmp <= 0;
+                valid_out_tmp <= 1;
             end
             else begin
                 //valid_out_tmp <= 0;
@@ -119,7 +116,6 @@ module axi_stream_insert_header #(
             end
         end
     end
-
     //assign ready_in = ~valid_out && ready_out_tmp ;
     assign ready_in = ~valid_out || output_over_flag;
     assign data_out = data_out_tmp;
@@ -127,17 +123,47 @@ module axi_stream_insert_header #(
     assign valid_out = valid_out_tmp;
     assign last_out = last_out_tmp;
     assign keep_out = keep_out_tmp;
+    //function automatic integer count_ones_4bit(input [3:0] data_in);
+        //integer cnt;
+        //begin
+            //cnt = 0;
+            //cnt = cnt + (data_in[0] & 1'b1);
+            //cnt = cnt + (data_in[1] & 1'b1);
+            //cnt = cnt + (data_in[2] & 1'b1);
+            //cnt = cnt + (data_in[3] & 1'b1);
+            //count_ones_4bit = cnt;
+        //end
+    //endfunction
     function automatic integer count_ones_4bit(input [3:0] data_in);
-        integer cnt;
-        begin
-            cnt = 0;
-            cnt = cnt + (data_in[0] & 1'b1);
-            cnt = cnt + (data_in[1] & 1'b1);
-            cnt = cnt + (data_in[2] & 1'b1);
-            cnt = cnt + (data_in[3] & 1'b1);
-            count_ones_4bit = cnt;
+    integer cnt;
+    begin
+        cnt = data_in[0];
+        cnt = cnt + data_in[1];
+        cnt = cnt + data_in[2];
+        cnt = cnt + data_in[3];
+        count_ones_4bit = cnt;
+    end
+    endfunction
+    //function automatic integer count_ones_4bit(input [3:0] data_in);
+        //integer cnt;
+        //integer i;
+        //begin
+            //cnt = 0;
+            //for (i = 0; i < DATA_BYTE_WD; i = i + 1) begin
+                //cnt = cnt + data_in[i];
+            //end
+            //count_ones_4bit = cnt;
+        //end
+    //endfunction
+    function automatic integer multiply(input [3:0]a,b);
+    integer i;
+    begin
+        multiply= 0;
+        for(i = 0; i < 4; i = i + 1)begin
+            if(b[i])begin
+                multiply = multiply + a  << i;
+            end
         end
+    end
     endfunction
 endmodule
-
-
